ae46a8feaf3cd0eaafe9d9b3489717203400efdc
Ludmila <liuda.schiopu@mail.ru>
2023-08-08 14:23:54 +0300

    .map in component Articles

--- src/Components/Articles/articles.js
+++ src/Components/Articles/articles.js
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import './articles.css';
 import Article from '../Article/article';
 
@@ -14,33 +15,77 @@ import article10 from './../../img/article10.png';
 import article11 from './../../img/article11.png';
 import article12 from './../../img/article12.png';
 
+
+const defaultArticles = [
+    {    
+        'text': "Here are some things you should know regarding how we work",
+        'img': article1,
+    },
+    {    
+        'text': "Granny gives everyone the finger, and other tips from OFFF Barcelona",
+        'img': article2,
+    },
+    {    
+        'text': "Hello world, or, in other words, why this blog exists",
+        'img': article3,
+    },
+    {    
+        'text': "Here are some things you should know regarding how we work",
+        'img': article4,
+    },
+    {    
+        'text': "Connecting artificial intelligence with digital product design",
+        'img': article5,
+    },
+    {    
+        'text': "It’s all about finding the perfect balance",
+        'img': article6,
+    },
+    {    
+        'text': "I believe learning is the most important skill",
+        'img': article7,
+    },
+    {    
+        'text': "Clients are part of the team",
+        'img': article8,
+    },
+    {    
+        'text': "Clients are part of the team",
+        'img': article9,
+    },
+    {    
+        'text': "Here are some things you should know regarding how we work",
+        'img': article10,
+    },
+    {    
+        'text': "Connecting artificial intelligence with digital product design",
+        'img': article11,
+    },
+    {    
+        'text': "How modern remote working tools get along with Old School Cowboy's methods",
+        'img': article12,
+    }
+]
+
+
+
 const Articles = () => {
+
+    const [articles, useArticles] = useState(defaultArticles);
     return (
         <div className='articles'>
-            <Article text="Here are some things you should know regarding how we work" link=""
-                     img={article1}/>
-            <Article text="Granny gives everyone the finger, and other tips from OFFF Barcelona"
-                     img={article2}/>
-            <Article text="Hello world, or, in other words, why this blog exists"
-                     img={article3}/>
-            <Article text="Here are some things you should know regarding how we work"
-                     img={article4}/>
-            <Article text="Connecting artificial intelligence with digital product design"
-                     img={article5}/>
-            <Article text="It’s all about finding the perfect balance"
-                     img={article6}/>
-            <Article text="I believe learning is the most important skill"
-                     img={article7}/>
-            <Article text="Clients are part of the team"
-                     img={article8}/>
-            <Article text="Clients are part of the team"
-                     img={article9}/>
-            <Article text="Here are some things you should know regarding how we work"
-                     img={article10}/>
-            <Article text="Connecting artificial intelligence with digital product design"
-                     img={article11}/>
-            <Article text="How modern remote working tools get along with Old School Cowboy's methods"
-                     img={article12}/>
+            {articles.map((article, i) => {
+                return <Article key={i} text={article.text} link=""
+                img={article.img}/>
+            })}
+
+
+
+
+
+
+
+            
         </div>
     )
 }
