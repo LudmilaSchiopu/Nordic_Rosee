90625e3dd9d3fc619ffda850b7270f96ffbe61f7
Ludmila <liuda.schiopu@mail.ru>
2023-09-18 14:35:22 +0300

    page1

---
+++ src/Components/Page1/page.js
@@ -0,0 +1,13 @@
+import Main from "../Main/main";
+import Articles from "../Articles/articles";
+
+const Page1 = () => {
+    return (
+        <div>
+            <Main/>
+            <Articles/>
+        </div>
+    )
+}
+
+export default Page1;
